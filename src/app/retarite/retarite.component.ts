e04adad4f3b53971b6f2cfecf7f8fde9fa98438e
zeddinieya <eya.zeddini@esen.tn>
2024-04-19 17:08:54 +0200

    partie front

---
+++ src/app/retarite/retarite.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retarite',
+  templateUrl: './retarite.component.html',
+  styleUrl: './retarite.component.css'
+})
+export class RetariteComponent {
+
+}
